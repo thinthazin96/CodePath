e260276c86e05ed144bb21c142d19acc81ffe49a
codingsake <thinthazin.chris@gmail.com>
2022-06-05 20:32:17 -0400

    fibonacci using loop

--- week-1/session-two.py
+++ week-1/session-two.py
@@ -27,6 +27,7 @@ if n = 1 -> 0
 if n = 2 -> 1
 """
 
+
 def fibonacci(n):
     assert n > 0
 
@@ -41,5 +42,35 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
+def fibonacci_iteration(n):
+    first_element = 0
+    second_element = 1
 
-print(fibonacci(6))
+    if n > 0:
+        #Check if the input is one number.
+        if n == 1:
+            #if yes, return 0
+            return first_element
+        
+        #Check if the input is two number.
+        if n == 2:
+            #if yes, return 1
+            return second_element
+
+        #if the input is more than two number,
+        else:
+            #loop from 3rd index through the input number + 1
+            for i in range(3, n+1):
+                #Store the additon of first_element and second_element in a result variable.
+                result = first_element + second_element
+                #Swap the second_element value to first_element
+                first_element = second_element
+                #Swap the result to the second_element
+                second_element = result
+
+            print(result)
+
+
+
+fibonacci_iteration(6)
+# print(fibonacci_iteration(6))
