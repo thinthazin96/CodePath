efb4cedee83266acd45fced7883fe0f78ad01640
Thin Thazin <thinthazin.chris@gmail.com>
2022-07-10 16:15:02 -0400

    Update 5.7.py

--- week-5/HK/5.7.py
+++ week-5/HK/5.7.py
@@ -1,3 +1,19 @@
+'''
+Complete the 'condense' function below.
+*
+* The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+* The function accepts INTEGER_SINGLY_LINKED_LIST head as parameter.
+*/
+
+/*
+* For your reference:
+*
+* SinglyLinkedListNode {
+*     int data;
+*     SinglyLinkedListNode next;
+* }
+'''
+
 def condense(head):
     hashSet = set()
     temp = head
