b22eb6b5bafce0835593b194a6b325ae8c45f200
codingsake <thinthazin.chris@gmail.com>
2022-07-30 12:16:04 -0400

    Added more exercies

--- Daily_Practice/week-one.py
+++ Daily_Practice/week-one.py
@@ -69,5 +69,62 @@ def validPalindrome(s):
         
         return True
 
-print(validPalindrome("A man, a plan, a canal: Panama"))
-print(validPalindrome("race a car"))
+# print(validPalindrome("A man, a plan, a canal: Panama"))
+# print(validPalindrome("race a car"))
+
+def appear_once(arr):
+
+    d = {}
+
+    for i in arr:
+        if i in d:
+            d[i] = d[i] + 1
+        else:
+            d[i] = 1
+
+    res = []
+    for key, value in d.items():
+        if value == 1:
+            res.append(key)
+            continue
+    return res
+
+# print("Appear Once: Test 1 => ", appear_once([1,1,2,2,3,4,4]) == [3])
+# print("Appear Once: Test 2 => ", appear_once([1,2,2,3,4,4]) == [1,3])
+# print("Appear Once: Test 3 => ", appear_once([1,2,3,4]) == [1,2,3,4])
+# print("Appear Once: Test 4 => ", appear_once([0]) == [0])
+# print("Appear Once: Test 5 => ", appear_once([]) == [])
+# print("Appear Once: Test 6 => ", appear_once([1,1,2,2,3,3,4,4]) == [])
+
+def count_gem(gems, stones):
+
+    if gems == [] or stones == []:              #if one of the list is empty, return None
+        return None 
+    
+    for gem in gems:
+        if gem in stones:
+    
+            gem_count = 0
+            d = {}
+
+            for stone in stones:
+                if stone in d:
+                    d[stone] = d[stone] + 1
+                else:
+                    d[stone] = 1
+
+            gems = set(gems)
+
+            for stone, n in d.items():
+                if stone in gems:
+                    gem_count += n
+            return gem_count    
+        else:
+            return None
+        
+# print("Gem_count in stone: Test 1 =>", count_gem(["G", "a"], ["g","g","g","G","A","a","a"]) == 3)
+# print("Gem_count in stone: Test 2 =>", count_gem([], ["g","g","g","G","A","a","a"]) == None)
+# print("Gem_count in stone: Test 3 =>", count_gem(["G", "a"], []) == None)
+# print("Gem_count in stone: Test 4 =>", count_gem(["G", "a"], ["g","g","g","A"]) == None)
+# print("Gem_count in stone: Test 5 =>", count_gem(["G", "a"], ["G"]) == 1)
+# print("Gem_count in stone: Test 6 =>", count_gem(["G"], ["G"]) == 1)
