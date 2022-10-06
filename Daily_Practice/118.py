f7435724f2e8c4798b89aaf60caf431a119874d6
codingsake <thinthazin.chris@gmail.com>
2022-10-06 01:38:19 -0400

    .

--- Daily_Practice/118.py
+++ Daily_Practice/118.py
@@ -1,9 +1,9 @@
 def generate(numRows):
     pascal = [[1]]
-    if numRows == 0:
+    if numRows == 0:            #if the input is 0, return empty list.
         return []
     
-    for i in range(numRows - 1):
+    for i in range(numRows - 1): 
         res = [1]
         for num in range(len(pascal[i])-1):
             res.append(pascal[i][num] + pascal[i][num+1])
