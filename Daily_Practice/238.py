1f2e24295588e6077e551f4cf06f70838e0f2599
codingsake <thinthazin.chris@gmail.com>
2022-12-27 14:17:54 -0500

    Product of Array Except Self

---
+++ Daily_Practice/238.py
@@ -0,0 +1,22 @@
+'''
+Product of Array Except Self: https://leetcode.com/problems/product-of-array-except-self/
+
+Time Complexity:
+'''
+
+def productExceptSelf(nums):
+    answer = [1] * len(nums)    # create array same length as input array by reserving 1
+    
+    prefix = 1                  # choose 1 value to neutralize
+    for i in range(len(nums)):
+        answer[i] = prefix      # whatever prefix value will update the current new array value
+        prefix *= nums[i]       # update the prefix value
+
+    postfix = 1
+    for i in range(len(nums)-1, -1, -1): #loop in reverse order
+        answer[i] *= postfix            #Overwrite the current value of new array by multiplying it with the postfix    
+        postfix *= nums[i]              #update the postfix value
+
+    return answer
+
+print(productExceptSelf([1,2,3,4]))
