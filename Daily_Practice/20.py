f393aeb38fb0ec340708e690dba927fae80d8844
thinthazin96 <thinthazin.chris@gmail.com>
2023-01-30 22:34:14 -0500

    Valid Parentheses

--- Daily_Practice/20.py
+++ Daily_Practice/20.py
@@ -1,21 +1,30 @@
 '''
 20. Valid Parentheses || CodePath Week6 Session 1
 
+Time Complexity: O(1)
 '''
 
 def isValid(s):
-    dic = {'(':')','{':'}','[':']'}
+    mapping = {')':'(','}':'{',']':'['}
     stack = []
     
-    for i in range(len(s)):
-        
-        for key, value in dic.items(): 
-            if s[i] in dict.keys():
-                stack.append(s[i])
-            else:
-                return False
-    return True
+    for paren in s:
+        if paren not in mapping.keys(): #opening bracket
+            stack.append(paren)
 
-print(isValid("()"))
+        else:                           #closing bracket    
+            if stack:                   #if the stack is not empty
+                top_element = stack.pop()   
+
+                if mapping[paren] != top_element:   #check if the current key's value and stack element is same.
+                    return False
+            else:                       #if the bracket start with closing bracket and the stack is empty
+                return  None            
+    return not stack        #if the stack is empty, return False
+
+print('Test 1: ', isValid("]") == None)
+print('Test 2: ', isValid("()") == True)
+print('Test 3: ', isValid("()[]{}") == True)
+print('Test 4: ', isValid("(]") == False)
 
 
