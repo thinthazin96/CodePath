4744a213e584b7fd4471e54fc909c8efb69e4cd1
codingsake <56745055+thinthazin96@users.noreply.github.com>
2023-02-01 23:35:39 -0500

    Revert "Generate Parentheses"
    This reverts commit 5ce899f272d746ff50d52c83aeb7acda02d94c65.

--- Daily_Practice/20.py
+++ Daily_Practice/20.py
@@ -1,27 +1,30 @@
 '''
-20. Valid Parentheses
+20. Valid Parentheses || CodePath Week6 Session 1
+
+Time Complexity: O(1)
 '''
 
 def isValid(s):
-  mapping = {'}': '{', ']': '[', ')' : '('}
-  stack = []
-
-  for paren in s:
-    if paren not in mapping.keys(): #Add opening in stack
-      stack.append(paren)
-    else:                          
-      if stack:                     #if the stack is not empty
-        
-        top_elem = stack.pop()
-  
-        if mapping[paren] != top_elem: #if the closing and opening doesn't match
-          return False 
-
-      else:
-        return False
-  return True
-
-print(isValid("("))
-
+    mapping = {')':'(','}':'{',']':'['}
+    stack = []
     
-    
+    for paren in s:
+        if paren not in mapping.keys(): #opening bracket
+            stack.append(paren)
+
+        else:                           #closing bracket    
+            if stack:                   #if the stack is not empty
+                top_element = stack.pop()   
+
+                if mapping[paren] != top_element:   #check if the current key's value and stack element is same.
+                    return False
+            else:                       #if the bracket start with closing bracket and the stack is empty
+                return  None            
+    return not stack        #if the stack is empty, return False
+
+print('Test 1: ', isValid("]") == None)
+print('Test 2: ', isValid("()") == True)
+print('Test 3: ', isValid("()[]{}") == True)
+print('Test 4: ', isValid("(]") == False)
+
+
