caf61c6fd365bd29e94116803dcc498f05d598dc
codingsake <56745055+thinthazin96@users.noreply.github.com>
2023-02-02 22:18:14 -0500

    Daily Temperatures

---
+++ Daily_Practice/739.py
@@ -0,0 +1,19 @@
+'''
+739. Daily Temperatures
+
+Time Complexity = O(n)
+
+'''
+def dailyTemperatures(temperatures):
+
+    stack = []                            # store pair:[temp, index]
+    res = [0] * len(temperatures)          #get the same length as given list and set 0 if case of no future day
+
+    for i, t in enumerate(temperatures):
+        while stack and t > stack[-1][0]: #as long as the stack is not empty and current temp > than the top element which is at 0 index -> stack[-1][0] 
+            stackT, stackInd = stack.pop() #pop pair:[temp, index]
+            res[stackInd] = (i - stackInd) #diff is the distance of index and store at that index position
+        stack.append((t, i))
+    return res
+
+print(dailyTemperatures([73,74,75,71,69,72,76,73]))
