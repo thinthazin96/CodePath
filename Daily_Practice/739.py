31ee1887cfb7576d92cb1821015f5cc38afba4fb
codingsake <56745055+thinthazin96@users.noreply.github.com>
2023-02-04 18:01:06 -0500

    Daily Temperatures

--- Daily_Practice/739.py
+++ Daily_Practice/739.py
@@ -10,7 +10,7 @@ def dailyTemperatures(temperatures):
     res = [0] * len(temperatures)          #get the same length as given list and set 0 if case of no future day
 
     for i, t in enumerate(temperatures):
-        while stack and t > stack[-1][0]: #as long as the stack is not empty and current temp > than the top element which is at 0 index -> stack[-1][0] 
+        while stack and t > stack[-1][0]: #as long as the stack is not empty and current temp > than the top element -> stack(temperature, index): stack[-1][0]
             stackT, stackInd = stack.pop() #pop pair:[temp, index]
             res[stackInd] = (i - stackInd) #diff is the distance of index and store at that index position
         stack.append((t, i))
