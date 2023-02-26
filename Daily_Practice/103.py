ac00b447bfcaf69c497eb787b2c4f55b4382fc78
codingsake <56745055+thinthazin96@users.noreply.github.com>
2023-02-25 22:35:42 -0500

    103. Binary Tree Zigzag Level Order Traversal

--- Daily_Practice/103.py
+++ Daily_Practice/103.py
@@ -1,8 +1,8 @@
 '''
 103. Binary Tree Zigzag Level Order Traversal
 
-Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
-
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. 
+(i.e., from left to right, then right to left for the next level and alternate between).
 
 Time Complexity: O(n)
 '''
