8bd63ea01ca97b2f5ff8396312d34ace58ba5ce1
thinthazin96 <thinthazin.chris@gmail.com>
2023-03-15 19:12:47 -0400

    One Edit Away

--- Daily_Practice/Day10.py
+++ Daily_Practice/Day10.py
@@ -1,10 +1,10 @@
 def oneEditAway(s1, s2):
-        if len(s1) >= len(s2): #base case
-            if len(s1) == len(s2): # pale, bale
-                return replace(s1, s2)
+        
+        if len(s1) == len(s2): # pale, bale
+            return replace(s1, s2)
 
-            if len(s1) > len(s2): #pales, pale
-                return insert(s1, s2)
+        elif len(s1) > len(s2): #pales, pale
+            return insert(s1, s2)
 
         elif len(s1) < len(s2): # ple, pale
             return remove(s1, s2)    
@@ -29,6 +29,8 @@ def replace(s1, s2):
 
 def insert(s1, s2):
     count, p1, p2 = 0, 0, 0
+    if len(s1) - len(s2) > 1:
+        return False
     while p1 < len(s1) and p2 < len(s2):
         if s1[p1] == s2[p2]:
             p1 += 1     
@@ -45,6 +47,8 @@ def remove(s1, s2):
     if not s2:          #if s2 is empty, return false
         return False
     
+    if len(s2) - len(s1) > 1:
+        return False
     count, p1, p2 = 0, 0, 0
     while p1 < len(s1) and p2 < len(s2):
         if s1[p1] == s2[p2]:
@@ -58,6 +62,12 @@ def remove(s1, s2):
                 return False
     return True
 
+print(oneEditAway("pale", "ple"))
+print(oneEditAway("pales", "pale"))
+print(oneEditAway("pale", "bale"))
+print(oneEditAway("pale", "bake"))
+
 # print(oneEditAway("pale", "bae"))
 # print(oneEditAway("aple", "apple"))
-print(oneEditAway("pale", "bake"))
+print(oneEditAway("doghouse", "dog"))
+# print(oneEditAway("dog", "doghouse"))
